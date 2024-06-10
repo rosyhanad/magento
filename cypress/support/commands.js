5a01542a1496a3e892a0c8ccf0f702b8a5e43b16
rosyhanad <roseehana8@gmail.com>
2024-06-10 19:44:14 +0800

    first commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,31 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('registrasi', (firstname, lastname, emailaddress, password, confirmpass) => {
+    cy.get('#firstname').type(firstname)
+    cy.get('#lastname').type(lastname)
+    cy.get('#email_address').type(emailaddress)
+    cy.get('#password').type(password)
+    cy.get('#password-confirmation').type(confirmpass)
+})
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
