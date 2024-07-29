81cd81923f39a8c8f7c44afc3d737ac8e6cc14b8
Ivan Armadi <103761586+HivanA98@users.noreply.github.com>
2024-07-29 21:15:29 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false, //some website hard to acceses because security of the chrome, this way to disable it
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
